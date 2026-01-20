a2ed71204387fd777215d32cc60ae4bd0c10262b
Myifan610 <475875412@qq.com>
2026-01-20 19:15:33 +0800

    Refactor argument handling in NettyRemotingClientSendSyncInterceptor

--- ext/apm-seata-skywalking-plugin/src/main/java/org/apache/seata/apm/skywalking/plugin/NettyRemotingClientSendSyncInterceptor.java
+++ ext/apm-seata-skywalking-plugin/src/main/java/org/apache/seata/apm/skywalking/plugin/NettyRemotingClientSendSyncInterceptor.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.seata.apm.skywalking.plugin;
 
 import com.alipay.sofa.common.profile.StringUtil;
@@ -36,6 +20,30 @@ import java.lang.reflect.Method;
 
 public class NettyRemotingClientSendSyncInterceptor implements InstanceMethodsAroundInterceptor {
 
+    private RpcMessage findRpcMessage(Object[] allArguments) {
+        if (allArguments == null) {
+            return null;
+        }
+        for (Object arg : allArguments) {
+            if (arg instanceof RpcMessage) {
+                return (RpcMessage) arg;
+            }
+        }
+        return null;
+    }
+
+    private Channel findChannel(Object[] allArguments) {
+        if (allArguments == null) {
+            return null;
+        }
+        for (Object arg : allArguments) {
+            if (arg instanceof Channel) {
+                return (Channel) arg;
+            }
+        }
+        return null;
+    }
+
     @Override
     public void beforeMethod(
             EnhancedInstance objInst,
@@ -44,12 +52,16 @@ public class NettyRemotingClientSendSyncInterceptor implements InstanceMethodsAr
             Class<?>[] argumentsTypes,
             MethodInterceptResult result)
             throws Throwable {
-        if (allArguments[0] == null) {
+        Channel channel = findChannel(allArguments);
+        if (channel == null) {
             return;
         }
-        Channel channel = (Channel) allArguments[0];
         String peer = SWSeataUtils.convertPeer(channel);
-        RpcMessage rpcMessage = (RpcMessage) allArguments[1];
+        RpcMessage rpcMessage = findRpcMessage(allArguments);
+        if (rpcMessage == null) {
+            return;
+        }
+
         String operationName = SWSeataUtils.convertOperationName(rpcMessage);
         ContextCarrier contextCarrier = new ContextCarrier();
         AbstractSpan activeSpan = ContextManager.createExitSpan(operationName, contextCarrier, peer);
@@ -72,8 +84,8 @@ public class NettyRemotingClientSendSyncInterceptor implements InstanceMethodsAr
     public Object afterMethod(
             EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Object ret)
             throws Throwable {
-        RpcMessage rpcMessage = (RpcMessage) allArguments[0];
-        if (rpcMessage.getBody() instanceof AbstractMessage) {
+        RpcMessage rpcMessage = findRpcMessage(allArguments);
+        if (rpcMessage != null && rpcMessage.getBody() instanceof AbstractMessage) {
             ContextManager.stopSpan();
         }
         return ret;
