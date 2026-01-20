3bd853cb81dc56c46f1b600f67f85d400585e6b6
Myifan610 <475875412@qq.com>
2026-01-20 19:16:57 +0800

    Add null checks for rpcMessage in utility methods

--- ext/apm-seata-skywalking-plugin/src/main/java/org/apache/seata/apm/skywalking/plugin/common/SWSeataUtils.java
+++ ext/apm-seata-skywalking-plugin/src/main/java/org/apache/seata/apm/skywalking/plugin/common/SWSeataUtils.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.seata.apm.skywalking.plugin.common;
 
 import io.netty.channel.Channel;
@@ -36,6 +20,9 @@ public class SWSeataUtils {
     }
 
     public static String convertOperationName(RpcMessage rpcMessage) {
+        if (rpcMessage == null || rpcMessage.getBody() == null) {
+            return ComponentsDefine.SEATA.getName();
+        }
         String requestSimpleName = rpcMessage.getBody().getClass().getSimpleName();
         if (SeataPluginConfig.Plugin.SEATA.SERVER) {
             return ComponentsDefine.SEATA.getName() + "/TC/" + requestSimpleName;
@@ -47,8 +34,15 @@ public class SWSeataUtils {
     }
 
     public static String convertXid(RpcMessage rpcMessage) {
+        if (rpcMessage == null || rpcMessage.getBody() == null) {
+            return null;
+        }
+        if (!(rpcMessage.getBody() instanceof AbstractMessage)) {
+            return null;
+        }
+
         AbstractMessage subMessage = (AbstractMessage) rpcMessage.getBody();
-        String requestSimpleName = rpcMessage.getBody().getClass().getSimpleName();
+        String requestSimpleName = subMessage.getClass().getSimpleName();
 
         String xid = null;
         try {
